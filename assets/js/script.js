d27896d8058784a6c6fe72190483803572ccf1be
Brandon Javillo <bjavillo1059@gmail.com>
2021-07-29 01:59:37 -0700

    completed html, started on readme, still adding to js for functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,72 @@
 // Global variable declaration
 var timeDisplayEl = $('#time-display');
-// var listedTimeGroup = $('.listed-time')
-var editBtn = $('.custom-btn')
+var displayTableEl = $('#display-table');
+var modalClickEl = $('#hourly-modal');
+var modalFormEl = $('#modal-form')
+var modalCloseEl = $('#modal-btn');
+var modalTitleEl = $('#modal-title-input');
+var modalDescriptionEl = $('#modal-description-input');
+var modalPriorityEl = $('modal-priority-input');
+var modalTaskStatusEl = $('modal-status-input');
+var modalSubmit = $('#submit-info');
+
 
 // displaying the time
-function displayCurrentTD() {
-    var currentTime = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
-    timeDisplayEl.text(currentTime);
-  }
-
-setInterval(displayCurrentTD, 1000);
-console.log(displayCurrentTD);
-
-editBtn.addEventListner('click', function() {
-  console.log(editBtn);
-
-});
-
-// When the user clicks on the button, open the modal
-editBtn.onclick = function() {
-  modal.style.display = "block";
+function displayDate() {
+  var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+  $('#time-display').text(currentTime);
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+// create element for modal --done
+// creat time input for modal --done
+// create title input for modal --done
+// create desciption input for modal --done
+// create priority dropdown for modal --done
+// create dropdown window for when time has past (need to add class color change to column when selected)
+// add a delete btn to remove input information from table --done
+// make modal disappear once info is input
+// 
+
+function displayModalData(time, title, description, priority) {
+  var displayTableRowEl = $('<tr>');
+
+  var displayTimeTdEl = $('<td>').addClass('p-2').text(time);
+
+  var displayTitleTdEl = $('<td>').addClass('p-2').text(title);
+
+  var displayDescriptTdEl = $('<td>').addClass('p-2').text(description);
+
+  var displayPriorityTdEl = $('<td>').addClass('p-2').text(priority);
+
+  var deleteInfoBtn = $('<td>')
+    .addClass('p-2 delete-info-btn text-center')
+    .text('X');
+
+displayTableRowEl.append(
+  displayTimeTdEl,
+  displayTitleTdEl,
+  displayDescriptTdEl,
+  displayPriorityTdEl,
+  deleteInfoBtn
+);
+
+displayTableEl.append(displayTableRowEl);
+
+modalClickEl.modal('hide');
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+// handle project form submission
+function modalFormSubmit(event) {
+  event.preventDefault();
+
+  var projectName = projectNameInputEl.val().trim();
+  var projectType = projectTypeInputEl.val().trim();
+  var hourlyRate = hourlyRateInputEl.val().trim();
+  var dueDate = dueDateInputEl.val().trim();
+
+  printProjectData(projectName, projectType, hourlyRate, dueDate);
+
+  projectFormEl[0].reset();
 }
+
+setInterval(displayDate, 1000);
