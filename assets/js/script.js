c81b533982f670acaa12555e5250afb6ceeeee75
Brandon Javillo <bjavillo1059@gmail.com>
2021-07-30 03:10:45 -0700

    added changes to javascript and html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,20 @@
 // Global variable declaration
 var timeDisplayEl = $('#time-display');
 var displayTableEl = $('#display-table');
+
 var modalClickEl = $('#hourly-modal');
 var modalFormEl = $('#modal-form')
 var modalCloseEl = $('#modal-btn');
+var modalTimeEl = $('#modal-time-input');
 var modalTitleEl = $('#modal-title-input');
 var modalDescriptionEl = $('#modal-description-input');
-var modalPriorityEl = $('modal-priority-input');
-var modalTaskStatusEl = $('modal-status-input');
+var modalPriorityEl = $('#modal-priority-input');
+var modalTaskStatusEl = $('#modal-status-input');
 var modalSubmit = $('#submit-info');
+var modalStatusArr = ["Completed", "Not Completed", "Time Remains", "On hold"]
+var modalTaskText = modalTaskStatusEl.text(modalStatusArr);
+
+
 
 
 // displaying the time
@@ -17,15 +23,71 @@ function displayDate() {
   $('#time-display').text(currentTime);
 }
 
+setInterval(displayDate, 1000);
+
+// date selected needs to be saved to calender link
 // create element for modal --done
 // creat time input for modal --done
 // create title input for modal --done
 // create desciption input for modal --done
 // create priority dropdown for modal --done
-// create dropdown window for when time has past (need to add class color change to column when selected)
-// add a delete btn to remove input information from table --done
+// create dropdown window for when time has past (need to add class color change to column when selected) --working
 // make modal disappear once info is input
-// 
+// make a color change to the task status block when a certain text is written
+
+
+
+function renderLastRegistered() {
+  var time = localStorage.getItem("time", time);
+  var title = localStorage.getItem("title", title);
+  var description = localStorage.getItem("description", description);
+  var priority = localStorage.getItem("priority", priority);
+  var status = localStorage.getItem("status", status);
+
+
+}
+
+modalFormEl.on('submit', function (e) {
+  e.preventDefault();
+  const modalData = {
+    time: modalTimeEl.val(),
+    title: modalTitleEl.val(),
+    description: modalDescriptionEl.val(),
+    priority: modalPriorityEl.val(),
+    status: modalTaskStatusEl.val()
+  }
+  console.log(modalData);
+
+  if (modalTaskText === 'Completed') {
+    $('#modal-status-input').addClass('text-white', 'bg-success');
+  } else if (modalTaskText === 'Not Completed') {
+    $('#modal-status-input').addClass('text-white', 'bg-danger');
+  } else if (modalTaskText === 'Time Remains') {
+    $('#modal-status-input').addClass('bg-warning');
+  } else (modalTaskText === 'On Hold')
+  $('#modal-status-input').addClass('text-white', 'bg-dark');
+
+
+
+  var time = modalTimeEl.val();
+  localStorage.setItem("time", time);
+
+  var title = modalTitleEl.val();
+  localStorage.setItem("title", title);
+
+  var description = modalDescriptionEl.val();
+  localStorage.setItem("description", description);
+
+  var priority = modalPriorityEl.val();
+  localStorage.setItem("priority", priority);
+
+  var status = modalTaskStatusEl.val();
+  localStorage.setItem("status", status);
+
+  renderLastRegistered();
+});
+
+
 
 function displayModalData(time, title, description, priority) {
   var displayTableRowEl = $('<tr>');
@@ -42,31 +104,23 @@ function displayModalData(time, title, description, priority) {
     .addClass('p-2 delete-info-btn text-center')
     .text('X');
 
-displayTableRowEl.append(
-  displayTimeTdEl,
-  displayTitleTdEl,
-  displayDescriptTdEl,
-  displayPriorityTdEl,
-  deleteInfoBtn
-);
+  displayTableRowEl.append(
+    displayTimeTdEl,
+    displayTitleTdEl,
+    displayDescriptTdEl,
+    displayPriorityTdEl,
+    deleteInfoBtn
+  );
 
-displayTableEl.append(displayTableRowEl);
+  displayTableEl.append(displayTableRowEl);
 
-modalClickEl.modal('hide');
+  modalClickEl.modal('hide');
 }
 
-// handle project form submission
-function modalFormSubmit(event) {
-  event.preventDefault();
-
-  var projectName = projectNameInputEl.val().trim();
-  var projectType = projectTypeInputEl.val().trim();
-  var hourlyRate = hourlyRateInputEl.val().trim();
-  var dueDate = dueDateInputEl.val().trim();
-
-  printProjectData(projectName, projectType, hourlyRate, dueDate);
-
-  projectFormEl[0].reset();
+function modalDeleteForm(event) {
+  console.log(event.target);
+  var btnClicked = $(event.target);
+  btnClicked.parent('tr').remove();
 }
 
-setInterval(displayDate, 1000);
+displayTableEl.on('click', '.delete-info-btn', modalDeleteForm);
